95829f5a6cac0d6b8d915016bfbaa54532fe186a
Nikita Gukov <isdelta@bk.ru>
2018-01-30 11:19:40 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-alert('Hello bro')
+var cycle1 = function() {
+    for (var i = 2; i <= 10; i++){
+        if  (i % 2 == 0){
+            alert(i)
+        }
+    }
+},
+cycle2 = function(){
+    var i = 0;
+    while (i<3){
+        alert( "номер" + i + "!" )
+        i++;
+    }
+},
+cycle3 = function() {
+    var num = 0;
+    while( num <100 ){
+      num = prompt ('Input num > 100', '')
+      if (num == null) break;
+    } 
+},
+cycle4 = function() {
+    for ( var i = 2; i< 10; i++){
+        for (var j = 2; j < i; j++){
+            if ( i % j ==0){
+                break
+            } 
+            alert(i);
+            break;
+        }   
+    }
+}
